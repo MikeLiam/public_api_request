3a3b0cb9de41cd5adb1072400db6c4cda00d6f97
MikeLiam <miguelalcalaromero@gmail.com>
2020-07-25 17:31:13 +0200

    intial structure fetching methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,40 @@
-console.log('holamundo');
+const employeesDiv = document.querySelector('#gallery');
+const employeesUrl = "https://randomuser.me/api/?results=12";
+
+async function fetchEmployees(url) {
+    function checkStatus(response) {
+        if (response.ok) {
+            return Promise.resolve(response);
+        } else {
+            return Promise.reject(new Error(response.statusText));
+        }
+    }
+    try {
+        return await fetch(url)
+                        .then(checkStatus)
+                        .then(data => data.json())
+                        .then(json => json.results);
+    } catch {
+        throw error;
+    }
+}
+
+// async function getEmployees(url) {
+//     const employeesJSON = await fetchEmployees(url);
+//     const cards = employeesJSON.results.map( employee => {
+//         return { ...}
+//     })
+// }
+
+function generateCards(data) {
+    data.map( employee => {
+        console.log(employee);
+    })
+}
+
+fetchEmployees(employeesUrl)
+    .then(generateCards)
+    .catch( e => {
+        employeesDiv.innerHTML = '<h3>Something went wrong!</h3>';
+        console.log(e);
+    });
